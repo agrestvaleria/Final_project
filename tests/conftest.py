2d30a1190595eb5d7b2a650c7053def7b624341e
Valeria Agrest <agrestval@gmail.com>
2022-06-09 17:30:04 +0300

    Final Project

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,6 +19,8 @@ from configs.db_parser import delete_group, delete_user, delete_auth_user_group
 def browser():
     chrome_options = Options()
     chrome_options.add_argument('--headless')
+    # chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-gpu')
     driver = webdriver.Chrome(options=chrome_options)
     driver.maximize_window()
     driver.implicitly_wait(10)
